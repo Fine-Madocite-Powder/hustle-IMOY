7742f2ad19289c6edd8b6b550a662c3ad04193db
Din Mamma <alexander.gustavsson@skola.taby.se>
2024-03-27 16:15:01 +0100

    vuybi

--- cringe_tekken.js
+++ cringe_tekken.js
@@ -1,8 +1,12 @@
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 
-function gameLoop() {
+function update() {
 
+
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    requestAnimationFrame(update)
 }
 
-setInterval(gameLoop, )
+requestAnimationFrame(update)
