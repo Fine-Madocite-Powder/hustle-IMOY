6f3b30e7e00f4501a65b21c2b865e5f61c837e03
Din Mamma <alexander.gustavsson@skola.taby.se>
2024-04-09 15:57:47 +0200

    Co-authored-by: RayGSkola <RayGSkola@users.noreply.github.com> fuyyfu

--- cringe_tekken.js
+++ cringe_tekken.js
@@ -1,11 +1,61 @@
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 
+// The "anim" object contains all sprites, spritesheets, and information that might be needed to animate the object.
+
+let anim = {
+    FireFist: {
+        frameWidth: 12,
+        frameHeight: 18,
+        frameCount: 1,
+        spriteSheet: document.getElementById("FireFist")
+    }
+}
+
+
+
+class Player {
+    constructor () {
+        this.health = 12;
+        this.grounded = true;
+        this.doublejump = true;
+        this.velX = 0;
+        this.velY = 0;
+        this.position = {
+            x: 350,
+            y: 200
+        }
+    }
+
+    fallAttack() {
+
+    }
+
+    standingAttack() {
+
+    }
+
+    moveAttack(){
+
+    }
+}
+
+
+
+let startTime = 0;
+let endTime = 0;
+let deltaTime = 0;
 function update() {
+    startTime = performance.now();
+    
+    console.log(deltaTime);
 
+    /*ctx.fillStyle = "red";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);*/
 
-    ctx.fillStyle = "black";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    endTime = performance.now();
+    deltaTime = endTime - startTime;
+
     requestAnimationFrame(update)
 }
 
