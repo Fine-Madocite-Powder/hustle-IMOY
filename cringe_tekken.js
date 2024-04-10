f20c87b64eacb93debf848afcff5277c39620606
Din Mamma <alexander.gustavsson@skola.taby.se>
2024-04-10 15:16:55 +0200

    Co-authored-by: RayGSkola <RayGSkola@users.noreply.github.com>

--- cringe_tekken.js
+++ cringe_tekken.js
@@ -1,30 +1,33 @@
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 
-// The "anim" object contains all sprites, spritesheets, and information that might be needed to animate the object.
-
+// --- The "anim" object contains all sprites, spritesheets, and information that might be needed to animate the object. ---
 let anim = {
     FireFist: {
         frameWidth: 12,
         frameHeight: 18,
         frameCount: 1,
         spriteSheet: document.getElementById("FireFist")
+    },
+    Background: {
+        spriteSheet: document.getElementById("background")
     }
 }
 
 
-
+// --- The "Player" class contains all information used to move and render both player characters. ---
 class Player {
-    constructor () {
+    constructor (x, y) {
         this.health = 12;
         this.grounded = true;
         this.doublejump = true;
         this.velX = 0;
         this.velY = 0;
         this.position = {
-            x: 350,
-            y: 200
+            x: x,
+            y: y
         }
+        this.facesright = true;
     }
 
     fallAttack() {
@@ -40,23 +43,31 @@ class Player {
     }
 }
 
+let player1 = new Player();
+let player2 = new Player();
 
 
-let startTime = 0;
-let endTime = 0;
-let deltaTime = 0;
+
+
+//let startTime = 0;
+//let endTime = 0;
+let deltaTime = 0; // Use deltaTime variable for physics calculations. It is very precise.
+
 function update() {
-    startTime = performance.now();
+    //startTime = performance.now();
+
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    ctx.drawImage(anim.Background.spriteSheet, 0, 0, canvas.width, canvas.height);
+
+
     
-    console.log(deltaTime);
 
-    /*ctx.fillStyle = "red";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);*/
+    //ctx.fillRect(20, 0, canvas.width, canvas.height);
 
-    endTime = performance.now();
-    deltaTime = endTime - startTime;
+    //endTime = performance.now();
+    //deltaTime = endTime - startTime;
 
-    requestAnimationFrame(update)
+    //requestAnimationFrame(update)
 }
 
 requestAnimationFrame(update);
